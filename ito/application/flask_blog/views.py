9d400893929285c77cb2a2753d6883ddf63a8274
1toutaich1 <itou.taichi.jd@mynavi.jp>
2024-05-28 00:36:16 +0000

    add flash

--- ito/application/flask_blog/views.py
+++ ito/application/flask_blog/views.py
@@ -1,5 +1,29 @@
+from flask import request,redirect,url_for,render_template,flash,session
 from flask_blog import app
+
 
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    if not session.get('logged_in'):
+        return redirect('/login')
+    return render_template('entries/index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            print('ユーザー名が異なります')
+            flash('ユーザー名が異なります')
+        elif request.form['password'] != app.config['PASSWORD']:
+            print('パスワードが異なります')
+            flash('パスワードが異なります')
+        else:
+            session['logged_in'] = True
+            flash('ログインしました')
+            return redirect('/')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in') # ログアウトしたらsession情報を削除
+    return redirect('/')
