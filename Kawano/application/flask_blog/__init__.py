aa6525251cca9e7c0fb8c6d7bb24ae722b6018ce
Taigakawano <kawano.taiga.mq@mynavi.jp>
2024-05-29 02:59:33 +0000

    finish create DataBase

--- Kawano/application/flask_blog/__init__.py
+++ Kawano/application/flask_blog/__init__.py
@@ -1,8 +1,15 @@
 '''アプリケーション本体ファイルの作成'''
 #Flask事態のインポート
 from flask import Flask
+#SQLアルケミーのインポート
+from flask_sqlalchemy import SQLAlchemy
+
 #Flaskアプリケーション本体を作成
 app = Flask(__name__)
 app.config.from_object('flask_blog.config')
+
+#db変数の設定
+db = SQLAlchemy(app)
+
 #viewsファイルのインポート
 import flask_blog.views
