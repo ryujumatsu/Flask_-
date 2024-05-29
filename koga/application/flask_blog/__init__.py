2eb2a136909f0610be37073e362d0364b04e5458
KeitaKoga <koga.keita.lj@mynavi.jp>
2024-05-29 02:58:27 +0000

    5/29日の午前中の演習分を追加

--- koga/application/flask_blog/__init__.py
+++ koga/application/flask_blog/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config.from_object('flask_blog.config') #コンフィグの有効化
+
+db=SQLAlchemy(app)
 
 import flask_blog.views
 
