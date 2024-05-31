c3c732cc732ec9263c7722410b028fcd11148525
ryujumatsu <matsumoto.ryuzyu.ut@mynavi.jp>
2024-05-31 00:59:24 +0000

    matsumoto: Introduce database.

--- matsumoto/application/flask_blog/views/views.py
+++ matsumoto/application/flask_blog/views/views.py
@@ -1,13 +1,6 @@
 from flask import flash, redirect, render_template, request, session, url_for
 from flask_blog import app
 
-@app.route("/")
-def show_entries():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
-    
-    return render_template("entries/index.html")
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
