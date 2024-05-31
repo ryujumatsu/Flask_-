213f9767bc2b8ec9d7629d4a2061bba37510fbb4
ryujumatsu <matsumoto.ryuzyu.ut@mynavi.jp>
2024-05-31 01:54:02 +0000

    matsumoto: Add login_required decorator.

--- matsumoto/application/flask_blog/views/views.py
+++ matsumoto/application/flask_blog/views/views.py
@@ -1,5 +1,17 @@
 from flask import flash, redirect, render_template, request, session, url_for
 from flask_blog import app
+from functools import wraps
+
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        return view(*args, **kwargs)
+    
+    return inner
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -14,6 +26,7 @@ def login():
             return redirect(url_for("show_entries"))
 
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
